37be09851a21702a3ce4823f95f687076894c1a4
Isaac Berdaky <isaac.berdaky@escola.pr.gov.br>
2025-08-14 07:50:02 -0300

    Updating Small changes

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # PorVoce_Suporte_Emocional_IA
 Criar um web app responsivo que oferece suporte emocional imediato em situações de crise, com triagem por IA de linguagem natural e encaminhamento automatizado. Baseado em Flask (Python) como backend principal.
+
+Coisas que minha mini-equipe está fazendo no update v2 branch Isaac / Gabriel
+
+UPDATE V1
+- SISTEMA DE LOGIN ACOPLADO no App.py login e registro html
+
+ATUALIZAÇÕES DE POLIMENTO QUE SERÃO NECESSÁRIAS FAZER NO FUTURO: 
+- Se o usuário já estiver logado, ele não pode acessar a pagina de login ou registro.
+
+
+UPDATE V2 (08/08)
+- Vamos começar a trabalhar no sistema de Triage e tals. Caixa de testes no chat de html
+- Api/triage sendo desenvolvido
--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 import uuid
 from functools import wraps
+from wordlists import low_risk, medium_risk, high_risk
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'chave_secretinha_owo'
@@ -17,6 +18,8 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
+login_manager.login_message = "Por favor, faça login para acessar esta página."
 
 # Banco de dados simples (apenas para login e registro)
 class Users(UserMixin, db.Model):
@@ -59,8 +62,12 @@ def dashboard(current_user):
     return render_template('index.html', username=current_user.username)
 
 
+#Atualizações futuras: Se o usuário já estiver logado, ele não pode acessar a pagina de login ou registro.
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+            return redirect(url_for('dashboard'))
+    
     if request.method == "POST":
         email = request.form.get("email")
         username = request.form.get("username")
@@ -87,6 +94,9 @@ def register():
     
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+            return redirect(url_for('dashboard'))
+    
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
@@ -129,8 +139,29 @@ def logout():
     flash('Você foi desconectado com sucesso.', 'success')
     return response
 
+# Verificação de status mental
+@app.route('/API/triage', methods=['POST'])
+def triage():
+    if request.method == 'POST':
+        message = request.form['chatTexto'].lower()
 
-
+        if any(word in message for word in low_risk):
+            response = "Parece que você está passando por um momento difícil. Se precisar, estou aqui para conversar."
+            return jsonify({"message": response, "user_message": message},
+                           {"risk":"low"})
+        
+        elif any(word in message for word in medium_risk):
+            response = "Sua mensagem indica que você pode estar enfrentando problemas sérios. É importante buscar ajuda profissional."
+            return jsonify({"message": response, "user_message": message},
+                           {"risk":"medium"})
+        
+        elif any(word in message for word in high_risk):
+            response = "Sua mensagem sugere que você pode estar em perigo. Por favor, procure ajuda imediatamente."
+            return jsonify({"message": response, "user_message": message},
+                           {"risk":"high"})
+    
+    return jsonify({"message": "Nenhum risco identificado.", "user_message": message},
+                   {"risk": "none"})
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
